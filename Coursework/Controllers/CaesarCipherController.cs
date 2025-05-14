8aa7520e4f7b0e45469d4b2a2e49bb9af902bc34
Бдайциев Сармат <bdaitsievsarmat@gmail.com>
2025-05-14 19:20:56 +0300

    добавлен шифр Цезаря

---
+++ Coursework/Controllers/CaesarCipherController.cs
@@ -0,0 +1,32 @@
+﻿using Coursework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coursework.Controllers
+{
+    public class CaesarCipherController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new CaesarCipher());
+        }
+
+        [HttpPost]
+        public ActionResult Index(CaesarCipher model, string action)
+        {
+            if (action == "encrypt")
+            {
+                model.OutputText = CaesarCipher.Encrypt(model.InputText, model.Key);
+            }
+            else if (action == "decrypt")
+            {
+                model.OutputText = CaesarCipher.Decrypt(model.InputText, model.Key);
+            }
+            else
+            {
+                model.OutputText = "Ошибка: выберите действие.";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Coursework/Models/CaesarCipher.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Coursework.Models
+{
+    public class CaesarCipher
+    {
+        [Required(ErrorMessage = "Введите текст")]
+        public string InputText { get; set; }
+
+        [Required(ErrorMessage = "Введите ключ")]
+        [Range(1, 33, ErrorMessage = "Ключ должен быть от 1 до 33")]
+        public int Key { get; set; }
+
+        public string OutputText { get; set; }
+
+        public static string Encrypt(string input, int key)
+        {
+            return ShiftText(input, key);
+        }
+
+        public static string Decrypt(string input, int key)
+        {
+            return ShiftText(input, -key);
+        }
+
+        public static string ShiftText(string input, int shift)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    char baseChar = char.IsUpper(c) ? 'А' : 'а';
+                    char shifted = (char)((((c - baseChar) + shift + 33) % 33) + baseChar);
+                    result.Append(shifted);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
