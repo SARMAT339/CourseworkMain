8863ae9cf6e77f9c31872091734f2f98b891ea70
Аслан <abazovaslan72@gmai.com>
2025-06-08 01:37:41 +0300

    Добавил Xor. Работает очень сыро,нужно будет допилить,чуток поигрался со стилями Home and Layout

---
+++ Coursework/Models/Xor.cs
@@ -0,0 +1,10 @@
+﻿namespace Coursework.Models
+{
+    public class Xor
+    {
+        public string InputText { get; set; }
+        public string Key { get; set; }
+        public string EncryptedText { get; set; }
+        public string DecryptedText { get; set; }
+    }
+}
