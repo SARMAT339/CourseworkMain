b38d603c691dccb9b6f9882e79aed1e7a752e033
Аслан <abazovaslan72@gmai.com>
2025-06-08 00:59:54 +0300

    Добавил шифр Плейфера. поддерживает русский и английский нужно корректировать шифровку и дешифровку русского.

---
+++ Coursework/Controllers/PlayfairController.cs
@@ -0,0 +1,34 @@
+﻿using Coursework.Models;
+using Microsoft.AspNetCore.Mvc;
+using PlayfairCipherApp.Services;
+
+namespace PlayfairCipherApp.Controllers
+{
+    public class PlayfairController : Controller
+    {
+        private readonly PlayfairService _playfairService;
+
+        public PlayfairController(PlayfairService playfairService)
+        {
+            _playfairService = playfairService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Playfair());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Playfair model, string language)
+        {
+            if (!string.IsNullOrEmpty(model.InputText))
+            {
+                model.EncryptedText = _playfairService.Encrypt(model.InputText, language);
+                model.DecryptedText = _playfairService.Decrypt(model.EncryptedText, language);
+            }
+
+            return View(model);
+        }
+    }
+}
--- Coursework/Program.cs
+++ Coursework/Program.cs
@@ -3,13 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Добавь эту строку, если используешь PlayfairService
+builder.Services.AddSingleton<PlayfairCipherApp.Services.PlayfairService>();
+
+// Если будешь использовать сессии
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Если будешь использовать куки или авторизацию
+builder.Services.AddHttpContextAccessor(); // Полезно для работы с HttpContext
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. 
     app.UseHsts();
 }
 
@@ -19,6 +32,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// Добавлено: поддержка сессий
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
